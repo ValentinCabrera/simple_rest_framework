1d6d57a5d0787d94baaaeedca69da00722b3da01
ValentinCabrera <valentincabrera2003@gmail.com>
2024-12-03 10:29:31 -0300

    feat: verision 0.4.1

--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_rest_framework',
-    version='0.1.1',
-    packages=find_packages(include=['simple_rest_framework', 'simple_rest_framework.*']),
+    version='0.4.1', 
+    packages=find_packages(),
     install_requires=[
-        'rest_framework',
+        'djangorestframework',
     ],
-    entry_points={
-        'console_scripts': [
-            'simple-rest=simple_rest_framework.cli:main',
-        ],
-    },
     author='Valentin Cabrera',
     author_email='valentincabrera2003@gmail.com',
     description='Esta libreria permite agilizar el desarrollo de APIs REST en Django. Manejando un CRUD básico de forma automática y manejando excepciones de forma sencilla.',
--- simple_rest_framework/simple_rest_framework/__init__.py
+++
@@ -1,3 +0,0 @@
-from exceptions import ServiceException, HandleExceptionsMixin
-from services import BaseService
-from views import BaseAPIView
